b2a66faf9dd534384a838c76b2414a3441c2d94d
WangHyungJun <whj2013123218@gmail.com>
2019-03-25 15:33:56 +0900

    Main Page Demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,212 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header id="header">
+      <div class="container clearfix">
+        <div class="row">
+          <section class="sub_header">
+            <div class="logo"><img src="../../public/img/jake.jpg"/></div>
+            <div class="sub_detail">After games 설명글이 들어갈 자리</div>
+            <div class="account">
+              <a href="#">로그인 | </a>
+              <a href="#">회원가입 </a>
+            </div>
+          </section>
+        </div>
+      </div>
+      <section class="Logo">
+        <div class="Logo_pic"><img src="../../public/img/jake.jpg"/></div>
+      </section>
+    </header>
+    <!--//header-->
+    <nav id="nav">
+      <div class="main_nav">
+        <ul class="clearfix">
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">이달의 게임</a></li>
+          <li><a href="#">LOL</a></li>
+          <li><a href="#">PUBG</a></li>
+          <li><a href="#">FIFA</a></li>
+          <li><a href="#">Hearh Stone</a></li>
+        </ul>
+      </div>
+      <div class="main_search">
+        <div class="search">
+          <input type="search"/>
+          <i class="fas fa-search"></i>
+        </div>
+      </div>
+    </nav>
+    <!--//nav-->
+    <section class="main">
+      <section class="Game">
+        <div class="Game_logo LOL clearfix">
+          <img src="../../public/img/LOL.jpg" alt="리그오브레전드"/>
+          <h3>리그오브레전드</h3>
+        </div>
+        <div class="container">
+          <div class="row clearfix">
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL1.jpg" alt="Lol1"/></div>
+              <h3>뉴스</h3>
+              <h3>눈길 가는 이색 종목</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL2.jpg" alt="Lol2"/></div>
+              <h3>유튜브</h3>
+              <h3>떴다! 8.16 패치노트</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL3.jpg" alt="Lol3"/></div>
+              <h3>블로그</h3>
+              <h3>라이엇게임즈, 롤을 잇는 후속작은 MMO?</h3>
+            </article>
+            <div class="Paging">
+              <img src="../../public/img/manplayinggame.jpg" alt="right"/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="Game greyground mt">
+        <div class="Game_logo LOL clearfix">
+          <img src="../../public/img/pubg.jpg" alt="PUBG"/>
+          <h3>배틀그라운드</h3>
+        </div>
+        <div class="container">
+          <div class="row clearfix">
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL1.jpg" alt="Lol1"/></div>
+              <h3>뉴스</h3>
+              <h3>눈길 가는 이색 종목</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL2.jpg" alt="Lol2"/></div>
+              <h3>유튜브</h3>
+              <h3>떴다! 8.16 패치노트</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL3.jpg" alt="Lol3"/></div>
+              <h3>블로그</h3>
+              <h3>라이엇게임즈, 롤을 잇는 후속작은 MMO?</h3>
+            </article>
+            <div class="Paging">
+              <img src="../../public/img/manplayinggame.jpg" alt="right"/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="Game mt">
+        <div class="Game_logo LOL clearfix">
+          <img src="../../public/img/하스스톤.png" alt="하스스톤"/>
+          <h3>하스스톤</h3>
+        </div>
+        <div class="container">
+          <div class="row clearfix">
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL1.jpg" alt="Lol1"/></div>
+              <h3>뉴스</h3>
+              <h3>눈길 가는 이색 종목</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL2.jpg" alt="Lol2"/></div>
+              <h3>유튜브</h3>
+              <h3>떴다! 8.16 패치노트</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL3.jpg" alt="Lol3"/></div>
+              <h3>블로그</h3>
+              <h3>라이엇게임즈, 롤을 잇는 후속작은 MMO?</h3>
+            </article>
+            <div class="Paging">
+              <img src="../../public/img/manplayinggame.jpg" alt="right"/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="Game greyground mt">
+        <div class="Game_logo LOL clearfix">
+          <img src="../../public/img/fifaonline.jpg" alt="FIFA"/>
+          <h3>피파온라인</h3>
+        </div>
+        <div class="container">
+          <div class="row clearfix">
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL1.jpg" alt="Lol1"/></div>
+              <h3>뉴스</h3>
+              <h3>눈길 가는 이색 종목</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL2.jpg" alt="Lol2"/></div>
+              <h3>유튜브</h3>
+              <h3>떴다! 8.16 패치노트</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL3.jpg" alt="Lol3"/></div>
+              <h3>블로그</h3>
+              <h3>라이엇게임즈, 롤을 잇는 후속작은 MMO?</h3>
+            </article>
+            <div class="Paging">
+              <img src="../../public/img/manplayinggame.jpg" alt="right"/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="Game mt">
+        <div class="Game_logo LOL clearfix">
+          <img src="../../public/img/pubg.jpg" alt="PUBG"/>
+          <h3>이달의 게임</h3>
+        </div>
+        <div class="container">
+          <div class="row clearfix">
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL1.jpg" alt="Lol1"/></div>
+              <h3>뉴스</h3>
+              <h3>눈길 가는 이색 종목</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL2.jpg" alt="Lol2"/></div>
+              <h3>유튜브</h3>
+              <h3>떴다! 8.16 패치노트</h3>
+            </article>
+            <article>
+              <div class="thumbnail"><img src="../../public/img/LOL3.jpg" alt="Lol3"/></div>
+              <h3>블로그</h3>
+              <h3>라이엇게임즈, 롤을 잇는 후속작은 MMO?</h3>
+            </article>
+            <div class="Paging">
+              <img src="../../public/img/manplayinggame.jpg" alt="right"/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+    <!--//main-->
+    <footer id="footer">
+      <div class="footer_content clearfix">
+        <div class="footer_logo">
+          <img src="../../public/img/jake.jpg" alt="aftergames"/>
+        </div>
+        <div class="footer_terms">
+          <a href="#">회사소개 |</a>
+          <a href="#">제품안내 |</a>
+          <a href="#">이용약관 |</a>
+          <a href="#">개인정보처리방침 |</a>
+          <a href="#">운영정책</a>
+        </div>
+        <div class="footer_email">
+          Aftergames@gf.co.kr
+        </div>
+      </div>
+    </footer>
+    <!--//footer-->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+
 };
 </script>
+
+<style lang="scss">
+  @import "../../public/scss/mainpage";
+</style>
